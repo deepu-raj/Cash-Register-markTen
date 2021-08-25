e501cffda3cc420c793c93da81df060dc82b84b3
deepu-raj <deepuraj.devan@gmail.com>
2021-08-25 23:56:18 +0530

    fix JS code
    value of td remains if the next input cash value is invalid. Solved by adding loop to create null array.

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const cash = document.querySelector("#cashamt");
 const checkBtn = document.querySelector("#check-btn");
 const output = document.querySelectorAll(".output");
 const error = document.querySelector("#errorMsg");
-
+const nullOutput = [];
 const denom = [2000,500,100,20,10,5,1];
 
 function initReturn() {
     const input = cash.value - billAmt.value;
     if ((cash.value)<(billAmt.value)){
         error.innerText = " Cash amount invalid.";
-    } else { 
+            for (let i=0;i<=denom.length;i++){
+                output[i].innerText = 0;
+            }
+        } else { 
         giveNoteDenom(input);
     }
 }
@@ -18,7 +21,7 @@ function initReturn() {
 function giveNoteDenom(input) {
     
     for (let i=0;i<=denom.length;i++){
-        const q = Math.floor(input/denom[i]);
+        const q = Math.trunc(input/denom[i]);
         input = input % denom[i];  
         output[i].innerText = q;
          }
